9725ea06547a70ac210d7a58a026cffdf319efd3
Ow <nikeow@yahoo.com>
2022-07-26 21:52:54 +0800

    Initial Commit

---
+++ aafafa/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  aafafa
+//
+//  Created by Mun Heng Ow on 26/07/2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ aafafa/aafafaApp.swift
@@ -0,0 +1,17 @@
+//
+//  aafafaApp.swift
+//  aafafa
+//
+//  Created by Mun Heng Ow on 26/07/2022.
+//
+
+import SwiftUI
+
+@main
+struct aafafaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
